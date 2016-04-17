1a2542d50f29e0f15e8932f956348acd275dc15f
Franco Ariel Salonia <franco_salonia@hotmail.com>
2016-04-17 19:04:57 -0300

    Agregado ejemplo django y vagrantfile

---
+++ django_example/snippets/serializers.py
@@ -0,0 +1,25 @@
+#The first part of the serializer class defines the fields that get serialized/deserialized.
+# The create() and update() methods define how fully fledged instances are created
+# or modified when calling serializer.save()
+
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
+
---
+++ django_example/snippets/views.py
@@ -0,0 +1,74 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+
+
+#The ONLY EntryPoint. This is great :)
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+# To take advantage of the fact that our responses are no longer hardwired to a single content type
+#  let's add support for format suffixes to our API endpoints. Using format suffixes gives us URLs
+#  that explicitly refer to a given format, and means our API will be able to handle URLs
+#  such as http://example.com/api/items/4/.json.
+
+# Notice that we've also used the @detail_route decorator to create a custom action, named highlight.
+# This decorator can be used to add any custom endpoints that don't fit into the standard create/update/delete style.
+
+# Custom actions which use the @detail_route decorator will respond to GET requests.
+# We can use the methods argument if we wanted an action that responded to POST requests.
+
+#The URLs for custom actions by default depend on the method name itself.
+# If you want to change the way url should be constructed, you can include url_path as a decorator keyword argument.
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
